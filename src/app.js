788bbc830d3a967fdc7d4cb84e9a9c8eafa13d67
Kameron Lund <klund-airtank@Kameron-Lund.local>
2022-07-01 09:25:13 -0600

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ console.log(__dirname)
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 const pubDirPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -102,6 +104,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000')
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
 })
